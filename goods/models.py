86cef8b8e2fa7244c08e43fb629568ab9f67765a
Andrey <kas190697@gmail.com>
2024-06-23 16:55:54 +0300

    Создание конвертера (slug) для url-адресов 'Products'.

--- goods/models.py
+++ goods/models.py
@@ -26,9 +26,10 @@ class Categories(models.Model):
         db_table = "category"
         verbose_name = "категорию"
         verbose_name_plural = "Категории"
-        
+
     def __str__(self):
         return self.name
+
 
 class Products(models.Model):
     """
@@ -49,12 +50,25 @@ class Products(models.Model):
     slug = models.CharField(
         max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
     )
-    descripion = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
-    price = models.DecimalField(default=0, max_digits=7, decimal_places=0, verbose_name='Стоимость в рублях')
-    discount = models.DecimalField(blank=True, null=True, default=0, max_digits=3, decimal_places=0, verbose_name='Скидка в %')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    descripion = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        default=0, max_digits=7, decimal_places=0, verbose_name="Стоимость в рублях"
+    )
+    discount = models.DecimalField(
+        blank=True,
+        null=True,
+        default=0,
+        max_digits=3,
+        decimal_places=0,
+        verbose_name="Скидка в %",
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Категория"
+    )
 
     class Meta:
         """Класс для отображения названий таблицы в БД и названия модели на русском языке.
@@ -67,17 +81,21 @@ class Products(models.Model):
         db_table = "product"
         verbose_name = "продукт"
         verbose_name_plural = "Продукты"
-        
+
     def __str__(self) -> str:
-        return f'{self.name}, количество - {self.quantity} шт.'
-    
+        """Метод для удобочитаемого отображения имени товара в админке."""
+        return f"{self.name}, количество - {self.quantity} шт."
+
     def display_id(self):
-        return f'Артикул товара - {self.id:005}'
+        """Метод, отображающий артикул товара на его карточке."""
+        return f"Артикул товара - {self.id:005}"
 
     def display_quantity(self):
-        return f'Штук в наличии - {self.quantity}' 
-    
+        """Метод, отображающий количество товара на его карточке."""
+        return f"Штук в наличии - {self.quantity}"
+
     def discount_calculation(self):
+        """Метод для корректного отображения стоимости товара со скидкой и без неё."""
         if self.discount:
-            return round(self.price * (100 - self.discount)/100)
+            return round(self.price * (100 - self.discount) / 100)
         return self.price
--- goods/views.py
+++ goods/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from goods.models import Products
 
 
-def product(request):
-    """Функция, отображающая шаблон product.html приложения goods."""
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    """Функция, отображающая шаблон product.html приложения goods.
+
+    Параметры переменной 'context':
+        product - информация о 'product_id' всех товаров из БД.
+    """
+
+    product = Products.objects.get(slug=product_slug)
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
 
 
 def catalog(request):
@@ -12,9 +19,9 @@ def catalog(request):
 
     Параметры переменной 'context':
         title - заголовок шаблона;
-        goods - информация о товаре, передаваемая из файла 'goods_list.py' в текущей директории.
+        goods - информация о всех товарах из БД.
     """
-    goods = Products.objects.all()
 
+    goods = Products.objects.all()
     context = {"title": "Home - Каталог", "goods": goods}
     return render(request, "goods/catalog.html", context)
