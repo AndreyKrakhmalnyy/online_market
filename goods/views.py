fcf7826086c5dd3afe135f1299a5d2dd98f65054
Andrey <kas190697@gmail.com>
2024-06-23 22:11:14 +0300

    Настройка пагинации товаров сервиса 'goods' через GET-параметр.

--- goods/views.py
+++ goods/views.py
@@ -15,38 +15,25 @@ def product(request, product_slug):
     return render(request, "goods/product.html", context=context)
 
 
-# def catalog(request, category_slug):
-#     """Функция отображает шаблон 'catalog.html' приложения 'goods' и возвращает информацию о товарах через переменную 'context'.
-
-#     Параметры переменной 'context':
-#         title - заголовок шаблона;
-#         goods - информация о всех товарах из БД.
-#     """
-#     if category_slug == 'all':
-#         goods = Products.objects.all()
-#     else:
-#         goods = Products.objects.filter(category__slug=category_slug)
-        
-#     context = {"title": "Home - Каталог", "goods": goods}
-#     return render(request, "goods/catalog.html", context)
-
-def catalog(request, category_slug, page=1):
-    """Функция отображает шаблон 'catalog.html' приложения 'goods' и возвращает информацию о товарах через переменную 'context'.
+def catalog(request, category_slug):
+    """Функция отображает шаблон 'catalog.html' сервиса 'goods' и возвращает информацию о товарах через переменную 'context'.
 
     Параметры переменной 'context':
         title - заголовок шаблона;
         goods - информация о всех товарах из БД.
     """
-    if category_slug == 'all-goods':
-        goods = Products.objects.all()
-    else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-        
+    page = request.GET.get('page', 1)
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     context = {
         "title": "Home - Каталог", 
         "goods": current_page,
         "slug_url": category_slug,
     }
+
+    if category_slug == 'all-goods':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        
     return render(request, "goods/catalog.html", context)
