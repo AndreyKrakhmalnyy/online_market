3bac3f30992d7418014d7cb5166b379214f8ae5f
Andrey <kas190697@gmail.com>
2024-06-24 17:37:39 +0300

    Добавления шаблонного тэга (change_parameters) в goods_tags для корректной пагинации по всем страницам в шаблоне catalog.html.

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,8 +1,34 @@
 from django import template
+from django.utils.http import urlencode
 from goods.models import Categories
 
 register = template.Library()
 
+
 @register.simple_tag()
 def tag_categories():
+    """
+    Возвращает queryset всех объектов модели.
+    
+    Returns:
+        QuerySet[Categories]: Список всех объектов модели Categories.
+    """
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_parameters(context, **kwargs):
+    """
+    Добавляет возможность корректной подгрузки и обновления страниц в разделе пагинации шаблона,
+    учитывая контекст.
+
+    Args:
+        context (dict): Текущий контекст запроса, включает в себя request.
+        *kwargs: Ключ-значение пары, которые будут добавлены в GET-параметры запроса.
+
+    Returns:
+        str: Закодированная строка URL, которая включает обновленные GET-параметры.
+    """
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -22,18 +22,31 @@ def catalog(request, category_slug):
         title - заголовок шаблона;
         goods - информация о всех товарах из БД.
     """
-    page = request.GET.get('page', 1)
+
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+
+    #Если 'slug' из таблицы 'Categories' равен 'all-goods', то отобразить все товары из БД, иначе отображает по выбранному значению 'slug'.
+    if category_slug == "all-goods":
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    #Если товар со скидкой ('on_sale'=True), то отобразить те товары, у которых 'discount' больше нуля."""
+    if on_sale:
+        goods = Products.objects.filter(discount__gt=0)
+        
+    #Если 'order_by'=True, то есть если галочка в фильтрн сортировки указана и не равна 'default', то отсортировать товары)."""
+    if order_by and order_by != "default":
+        goods = Products.objects.order_by(order_by)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
     context = {
-        "title": "Home - Каталог", 
+        "title": "Home - Каталог",
         "goods": current_page,
         "slug_url": category_slug,
     }
 
-    if category_slug == 'all-goods':
-        goods = Products.objects.all()
-    else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-        
     return render(request, "goods/catalog.html", context)
