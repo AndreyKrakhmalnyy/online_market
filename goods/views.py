141e41a4633f88d7f525d22a58f5b491a0bf397f
Andrey <kas190697@gmail.com>
2024-07-28 15:32:05 +0300

    Перевод контроллеров из main и goods на представления django.

--- goods/views.py
+++ goods/views.py
@@ -1,80 +1,136 @@
 from typing import Any
-from django.core.paginator import Paginator
-from django.http import Http404, HttpResponse
-from django.shortcuts import get_list_or_404, render
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.views.generic import DetailView, ListView
 from goods.models import Products
 from goods.utils import q_search
+from django.http import Http404
 
 
-def product(request, product_slug: str):
-    """Отображает шаблон 'product.html' приложения 'goods'.
+class ProductView(DetailView):
+    template_name = "goods/product.html"
+    context_object_name = "product"
+    slug_url_kwarg = "product_slug"
+    allow_empty = False
+    
+    def get_object(self, queryset=None) -> Model:
+        product = Products.objects.get(slug=self.kwargs.get(self.slug_url_kwarg))
+        return product
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.name
+        return context
 
-    Args:
-        request: Запрос пользователя.
-        product_slug (str): slug товара.
+class CatalogView(ListView):
+    # model = Products
+    queryset = Products.objects.all().order_by("-id")
+    context_object_name = "goods"
+    template_name = "goods/catalog.html"
+    paginate_by = 3
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        category_slug = self.kwargs.get("category_slug")
+        on_sale = self.request.GET.get("on_sale")
+        order_by = self.request.GET.get("order_by")
+        query = self.request.GET.get("q")
 
-    Attributes:
-        product: Запрашиваемый объект товара из БД по его slug.
-        context (dict[str, Products]): Словарь, содержащий объект товара для передачи в шаблон.
+        if category_slug == "all-goods":
+            goods = super().get_queryset()
+        elif query:
+            goods = q_search(query)
+        else:
+            goods = super().get_queryset().filter(category__slug=category_slug)
+            if not goods.exists():
+                raise Http404()
 
-    Returns:
-        HttpResponse: Ответ, отображающий шаблон 'goods/product.html' с контекстом, содержащим объект товара.
-    """
+        if on_sale:
+            goods = super().get_queryset().filter(discount__gt=0)
 
-    product: Products = Products.objects.get(slug=product_slug)
-    context: dict[str, Products] = {"product": product}
+        if order_by and order_by != "default":
+            goods = super().get_queryset().order_by(order_by)
+        
+        return goods
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Каталог"
+        context['slug_url'] = self.kwargs.get("category_slug")
+        return context
+    
 
-    return render(request, "goods/product.html", context=context)
 
 
-def catalog(request, category_slug: str = None) -> HttpResponse:
-    """Отображает шаблон 'catalog.html' сервиса 'goods'.
 
-    Обрабатывает GET-параметры запроса для фильтрации и сортировки товаров.
 
-    Args:
-        request: Запрос пользователя.
-        category_slug: 'slug' категории (если задан).
+# def product(request, product_slug: str):
+#     """Отображает шаблон 'product.html' приложения 'goods'.
 
-    Attributes:
-        page: Номер страницы для пагинации (по умолчанию 1).
-        on_sale: Флаг, указывающий на то, нужно ли показывать только товары со скидкой (по умолчанию None).
-        order_by: Критерий сортировки (по умолчанию None).
-        query: Поисковый запрос (по умолчанию None).
-        goods: QuerySet с товарами, отфильтрованными и отсортированными по параметрам запроса.
-        paginator: Объект 'Paginator' для пагинации товаров.
-        current_page: Текущая страница пагинации.
-        context (dict[str, Any]): Словарь, содержащий данные для шаблона 'catalog.html'.
+#     Args:
+#         request: Запрос пользователя.
+#         product_slug (str): slug товара.
 
-    Returns:
-        HttpResponse: Ответ, отображающий шаблон 'catalog.html' с контекстом фильтрации и пагинации.
-    """
-    page = request.GET.get("page", 1)
-    on_sale = request.GET.get("on_sale", None)
-    order_by = request.GET.get("order_by", None)
-    query = request.GET.get("q", None)
+#     Attributes:
+#         product: Запрашиваемый объект товара из БД по его slug.
+#         context (dict[str, Products]): Словарь, содержащий объект товара для передачи в шаблон.
 
-    if category_slug == "all-goods":
-        goods = Products.objects.all()
-    elif query:
-        goods = q_search(query)
-    else:
-        goods = Products.objects.filter(category__slug=category_slug)
-        if not goods.exists():
-            raise Http404()
+#     Returns:
+#         HttpResponse: Ответ, отображающий шаблон 'goods/product.html' с контекстом, содержащим объект товара.
+#     """
 
-    if on_sale:
-        goods = Products.objects.filter(discount__gt=0)
+#     product: Products = Products.objects.get(slug=product_slug)
+#     context: dict[str, Products] = {"product": product}
 
-    if order_by and order_by != "default":
-        goods = Products.objects.order_by(order_by)
+#     return render(request, "goods/product.html", context=context)
 
-    paginator = Paginator(goods, 3)
-    current_page = paginator.page(int(page))
-    context: dict[str, Any] = {
-        "title": "Каталог товаров",
-        "goods": current_page,
-        "slug_url": category_slug,
-    }
+# def catalog(request, category_slug: str = None) -> HttpResponse:
+#     """Отображает шаблон 'catalog.html' сервиса 'goods'.
 
-    return render(request, "goods/catalog.html", context)
+#     Обрабатывает GET-параметры запроса для фильтрации и сортировки товаров.
+
+#     Args:
+#         request: Запрос пользователя.
+#         category_slug: 'slug' категории (если задан).
+
+#     Attributes:
+#         page: Номер страницы для пагинации (по умолчанию 1).
+#         on_sale: Флаг, указывающий на то, нужно ли показывать только товары со скидкой (по умолчанию None).
+#         order_by: Критерий сортировки (по умолчанию None).
+#         query: Поисковый запрос (по умолчанию None).
+#         goods: QuerySet с товарами, отфильтрованными и отсортированными по параметрам запроса.
+#         paginator: Объект 'Paginator' для пагинации товаров.
+#         current_page: Текущая страница пагинации.
+#         context (dict[str, Any]): Словарь, содержащий данные для шаблона 'catalog.html'.
+
+#     Returns:
+#         HttpResponse: Ответ, отображающий шаблон 'catalog.html' с контекстом фильтрации и пагинации.
+#     """
+    # page = request.GET.get("page", 1)
+    # on_sale = request.GET.get("on_sale", None)
+    # order_by = request.GET.get("order_by", None)
+    # query = request.GET.get("q", None)
+
+    # if category_slug == "all-goods":
+    #     goods = Products.objects.all()
+    # elif query:
+    #     goods = q_search(query)
+    # else:
+    #     goods = Products.objects.filter(category__slug=category_slug)
+    #     if not goods.exists():
+    #         raise Http404()
+
+    # if on_sale:
+    #     goods = Products.objects.filter(discount__gt=0)
+
+    # if order_by and order_by != "default":
+    #     goods = Products.objects.order_by(order_by)
+
+#     paginator = Paginator(goods, 3)
+#     current_page = paginator.page(int(page))
+#     context: dict[str, Any] = {
+#         "title": "Каталог товаров",
+#         "goods": current_page,
+#         "slug_url": category_slug,
+#     }
+
+#     return render(request, "goods/catalog.html", context)
--- main/views.py
+++ main/views.py
@@ -1,45 +1,63 @@
-from typing import Any
-from django.shortcuts import render
-from goods.models import Categories
+from django.views.generic import TemplateView
 
 
-def index(request):
-    """Отображает шаблон и возвращает информацию о товарах.
-
-    Args:
-        request: Запрос пользователя.
-
-    Attributes:
-        categories: QuerySet со всеми категориями товаров.
-        context (dict[str, Any]): Словарь, содержащий данные для шаблона 'main/index.html'.
-
-    Returns:
-        HttpResponse: Ответ, отображающий шаблон 'main/index.html' с контекстом, содержащим информацию о категориях.
-    """
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
     
-    categories = Categories.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        context['content'] = 'Добро пожаловать в магазин мебели HomeLand!'
+        return context
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
     
-    context: dict[str, Any]  = {
-        'title': 'Главная',
-        'content': 'Добро пожаловать в магазин мебели HomeLand!',
-        'categories': categories
-    }
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Общая информация'
+        context['content'] = 'О нас'
+        return context
 
-def about(request):
-    """Отображает шаблон и возвращает информацию о странице.
 
-    Args:
-        request: Запрос пользователя.
 
-    Attributes:
-        context (dict[str, Any]): Словарь, содержащий данные для шаблона 'main/about.html'.
+# def index(request):
+#     """Отображает шаблон и возвращает информацию о товарах.
 
-    Returns:
-        HttpResponse: Ответ, отображающий шаблон 'main/about.html' с контекстом, содержащим заголовок и контент страницы.
-    """
-    context: dict[str, Any] = {
-        'title': 'Общая информация',
-        'content': 'О нас',
-    }
-    return render(request, 'main/about.html', context)
+#     Args:
+#         request: Запрос пользователя.
+
+#     Attributes:
+#         categories: QuerySet со всеми категориями товаров.
+#         context (dict[str, Any]): Словарь, содержащий данные для шаблона 'main/index.html'.
+
+#     Returns:
+#         HttpResponse: Ответ, отображающий шаблон 'main/index.html' с контекстом, содержащим информацию о категориях.
+#     """
+    
+#     categories = Categories.objects.all()
+    
+#     context: dict[str, Any]  = {
+#         'title': 'Главная',
+#         'content': 'Добро пожаловать в магазин мебели HomeLand!',
+#         'categories': categories
+#     }
+#     return render(request, 'main/index.html', context)
+
+# def about(request):
+#     """Отображает шаблон и возвращает информацию о странице.
+
+#     Args:
+#         request: Запрос пользователя.
+
+#     Attributes:
+#         context (dict[str, Any]): Словарь, содержащий данные для шаблона 'main/about.html'.
+
+#     Returns:
+#         HttpResponse: Ответ, отображающий шаблон 'main/about.html' с контекстом, содержащим заголовок и контент страницы.
+#     """
+#     context: dict[str, Any] = {
+#         'title': 'Общая информация',
+#         'content': 'О нас',
+#     }
+#     return render(request, 'main/about.html', context)
